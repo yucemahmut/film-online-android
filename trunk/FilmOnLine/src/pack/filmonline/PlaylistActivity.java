6d3b5cc3a16f0a894d2792589b2f97ab457758d7
a.s.hereb@gmail.com <a.s.hereb@gmail.com@d3da216f-c43f-d7db-d150-d08823ada09d>
2012-02-05 01:48:40 +0000

    Version 2.0 - BETA

--- trunk/FilmOnLine/src/pack/filmonline/PlaylistActivity.java
+++ trunk/FilmOnLine/src/pack/filmonline/PlaylistActivity.java
@@ -2,26 +2,35 @@ package pack.filmonline;
 
 import io.pen.bluepixel.filmonline.R;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -32,6 +41,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class PlaylistActivity extends ListActivity {
+	private static final String CONTACT_MAIL = "film.online.android@gmail.com";
 	private String playlistID = "";
 	private final String PART_TAG = "Playlist - Parte ";
 	private List<String> TITLES = new ArrayList<String>();
@@ -45,45 +55,45 @@ public class PlaylistActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.playlist);
-		try {
-			TITLES = (List) savedInstanceState.getSerializable(TITLES_LABEL);
-			URLS = (List) savedInstanceState.getSerializable(URLS_LABEL);
-			buildList();
-		} catch (Exception e) {
-			loadList();
-		}	
-		//Set title
-		Bundle extras = getIntent().getExtras(); 
-		TextView tv = (TextView) findViewById(R.id.playlistTitle);
-		tv.setText(extras.getString(FilmCompletiActivity.TITLE_LABEL));
-		
-		//Set listeners
-		final ImageButton backBtn = (ImageButton) findViewById(R.id.playlistBack);
-		backBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
-	}
+		try{
+			try {
+				TITLES = (List) savedInstanceState.getSerializable(TITLES_LABEL);
+				URLS = (List) savedInstanceState.getSerializable(URLS_LABEL);
+				buildList();
+			} catch (Exception e) {
+				loadList();
+			}	
+			//Set title
+			Bundle extras = getIntent().getExtras(); 
+			TextView tv = (TextView) findViewById(R.id.playlistTitle);
+			tv.setText(extras.getString(FilmCompletiActivity.TITLE_LABEL));
 
-	private void apiCall(){
-		//Get playlist ID
-		Bundle extras = getIntent().getExtras(); 
-		playlistID = extras.getString(FilmCompletiActivity.PLAYLISTID_LABEL);
-		Document doc = null;
-		//Get playlist parts 
-		try {
-			URL url = new URL("http://gdata.youtube.com/feeds/api/playlists/"+playlistID+"?v=2");
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			doc = db.parse(new InputSource(url.openStream()));
-			doc.getDocumentElement().normalize();
+			//Set listeners
+			final ImageButton backBtn = (ImageButton) findViewById(R.id.playlistBack);
+			backBtn.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					finish();
+				}
+			});			
 		} catch (Exception e) {
 			//System.out.println("XML Pasing Excpetion = " + e);
 			setResult(FilmCompletiActivity.RESULTCODE_KO);
 			finish();
 		}
+	}
+
+	private void apiCall() throws ParserConfigurationException, SAXException, IOException{
+		//Get playlist ID
+		Bundle extras = getIntent().getExtras(); 
+		playlistID = extras.getString(FilmCompletiActivity.PLAYLISTID_LABEL);
+		Document doc = null;
+		//Get playlist parts 
+		URL url = new URL("http://gdata.youtube.com/feeds/api/playlists/"+playlistID+"?v=2");
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		doc = db.parse(new InputSource(url.openStream()));
+		doc.getDocumentElement().normalize();
 		NodeList nodeList = doc.getElementsByTagName("yt:videoid");
 		for (int i = 0; i < nodeList.getLength(); ++i) {
 			int index = i+1;
@@ -135,12 +145,44 @@ public class PlaylistActivity extends ListActivity {
 		URLS = (List) savedInstanceState.getSerializable(URLS_LABEL);
 	}
 
+	private void openUri(Uri uri) {
+		Intent i = new Intent(Intent.ACTION_VIEW, uri);
+		startActivity(i);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.opt_menu_playlist, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle menu selections
+		switch (item.getItemId()) {
+		case R.id.opt_back:
+			//Go back
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 
 	protected class InitTask extends AsyncTask<Context, Integer, String> {
 		// Background task to parse the remote XML list, while showing a nice "Loading" dialog.
 		@Override
 		protected String doInBackground(Context... params) {
-			apiCall();
+			try{
+				apiCall();
+			} catch (Exception e) {
+				//System.out.println("XML Pasing Excpetion = " + e);
+				setResult(FilmCompletiActivity.RESULTCODE_KO);
+				progDailog.dismiss();
+				finish();
+			}
 			return "COMPLETE!";
 		}
 
